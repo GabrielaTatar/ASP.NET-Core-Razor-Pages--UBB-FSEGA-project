2f735870b17d59a317f596d17f8d18b3452c6288
GabrielaTatar <gabriela.tatar07@gmail.com>
2022-11-12 16:15:28 +0200

    Tema - lab04

---
+++ Tatar_Gabriela_Lab02/Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Tatar_Gabriela_Lab02.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Tatar_Gabriela_Lab02/Pages/Categories/Index.cshtml.cs
+++ Tatar_Gabriela_Lab02/Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Tatar_Gabriela_Lab02.Data;
 using Tatar_Gabriela_Lab02.Models;
+using Tatar_Gabriela_Lab02.Models.ViewModels;
 
 namespace Tatar_Gabriela_Lab02.Pages.Categories
 {
@@ -19,15 +20,31 @@ namespace Tatar_Gabriela_Lab02.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
+        public IList<Category> Category { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(c => c.Book)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
-                
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book);
             }
+
+
+
         }
     }
 }
+
